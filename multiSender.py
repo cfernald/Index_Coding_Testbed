ba702e265ed8f1b8af726c838f96ba16c0fc3d96
Ryan Bridges <rybridges90@gmail.com>
2015-10-26 16:22:22 -0500

    blindly copy and paste the multicasters

---
+++ multiSender.py
@@ -0,0 +1,14 @@
+# UDP multicast examples, Hugo Vincent, 2005-05-14.
+import socket
+
+def send(data, port=50000, addr='239.192.1.100'):
+	"""send(data[, port[, addr]]) - multicasts a UDP datagram."""
+	# Create the socket
+	s = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+	# Make the socket multicast-aware, and set TTL.
+	s.setsockopt(socket.IPPROTO_IP, socket.IP_MULTICAST_TTL, 20) # Change TTL (=20) to suit
+	# Send the data
+	s.sendto(data, (addr, port))
+
+if __name__ == "__main__":
+	send("hi from me")
