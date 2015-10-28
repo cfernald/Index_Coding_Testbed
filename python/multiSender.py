a3184b2a2d7d38342bf2ac143ee1be6729ad5a58
Chris Fernald <chris.fernald@utexas.edu>
2015-10-28 13:07:31 -0500

    reorganized the repo

--- python/multiSender.py
+++ python/multiSender.py
@@ -1,7 +1,7 @@
 # UDP multicast examples, Hugo Vincent, 2005-05-14.
 import socket
 
-def send(data, port=50000, addr='239.192.1.100'):
+def send(data, port=50000, addr='10.42.0.1'):
 	"""send(data[, port[, addr]]) - multicasts a UDP datagram."""
 	# Create the socket
 	s = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
