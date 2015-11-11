270a54017f93af819f81d4d7b8b9f3c41e19e0dd
Chris Fernald <chris.fernald@utexas.edu>
2015-11-11 12:18:35 -0600

    added more general code

--- python/rec_broadcast.py
+++ python/rec_broadcast.py
@@ -1,9 +1,13 @@
 import select, socket 
+from udp import *
 
 port = 50000  # where do you expect to get a msg?
 bufferSize = 1024 # whatever you need
 
-s = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+rec = UdpReceiver(port)
+while True:
+  print rec.rec(bufferSize)
+'''s = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
 s.bind(('', port))
 s.setblocking(0)
 
@@ -11,4 +15,4 @@ while True:
   result = select.select([s],[],[])
   msg = result[0][0].recv(bufferSize) 
   print msg
-
+'''
--- python/udp.py
+++ python/udp.py
@@ -1,20 +1,29 @@
-import sys
 from socket import *
+import select
 
 class UdpBroadcaster:
   'Sends broadcast messages to network'
   addr = ''
   
   def __init__(self, addr):
-    self.soc = socket(AF_INET, SOCK_DGRAM)
-    self.soc.bind((addr, 0))
-    self.soc.setsockopt(SOL_SOCKET, SO_BROADCAST, 1)
+    self.sock = socket(AF_INET, SOCK_DGRAM)
+    self.sock.bind((addr, 0))
+    self.sock.setsockopt(SOL_SOCKET, SO_BROADCAST, 1)
 
   def send(self, message, port):
-    self.soc.sendto(message, ('<broadcast>', port))
+    self.sock.sendto(message, ('<broadcast>', port))
 
-'''class UdpReceiver:
+#import socket, select
+
+class UdpReceiver:
   'Receives UDP packets on the network'
 
-  def __init__(self, addr, port):
-  '''
+  def __init__(self, port):
+    self.sock = socket(AF_INET, SOCK_DGRAM)
+    self.sock.bind(('', port))
+    self.sock.setblocking(0)
+
+  def rec(self, bufferSize):
+    result = select.select([self.sock], [], [])
+    return result[0][0].recv(bufferSize)
+
