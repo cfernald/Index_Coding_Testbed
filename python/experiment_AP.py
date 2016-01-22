1d5cc68b46e22ec9e3596b5cf9f471c74b3611c0
Chris Fernald <chris.fernald@utexas.edu>
2016-01-22 01:25:35 -0600

    added some ssh stuff and got the start script working

--- python/experiment_AP.py
+++ python/experiment_AP.py
@@ -5,16 +5,25 @@ import socket
 import thread
 import ack_handler
 import algorithms
+import signal 
+import sys
 from time import sleep,time
 
 # Static Variables
-messages = ["2"]
+messages = ["0"]
 PORT = 5000
 MY_IP = '10.42.0.1'
 
 # setup the ack listener
 acks = ack_handler.AckListener(len(messages))
 acks.start()
+
+# setup shutdown listener
+def signal_handler(signal, frame):
+    print "Shutting down...\n"
+    acks.stop()
+    sys.exit(0)
+signal.signal(signal.SIGINT, signal_handler)
 
 # initial round
 broadcaster = udp.UdpBroadcaster(MY_IP)
