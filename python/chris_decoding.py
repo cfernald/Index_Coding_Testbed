262208e343043fe5d674d318a3c8ee35e3da6f15
Chris Fernald <chris.fernald@utexas.edu>
2016-03-13 13:42:19 -0500

    A simple decoding test and some various scripts changes

---
+++ python/chris_decoding.py
@@ -0,0 +1,28 @@
+
+def gauss(A):
+    n = len(A)
+
+    for i in range(0, n):
+        # Search for maximum in this column
+        maxEl = abs(A[i][i])
+        maxRow = i
+        for k in range(i+1, n):
+            if abs(A[k][i]) > maxEl:
+                maxEl = abs(A[k][i])
+                maxRow = k
+
+        # Swap maximum row with current row (column by column)
+        for k in range(i, n+1):
+            tmp = A[maxRow][k]
+            A[maxRow][k] = A[i][k]
+            A[i][k] = tmp
+
+        # Make all rows below this one 0 in current column
+        for k in range(i+1, n):
+
+            for j in range(i, n+1):
+
+                if i == j:
+                    A[k][j] = 0
+                else:
+                    A[k][j] = (A[k][j] * A[i][i]) - (A[i][j] * A[k][i])
--- python/experiment_AP.py
+++ python/experiment_AP.py
@@ -9,14 +9,16 @@ import sys
 import messages
 from time import sleep,time
 import matplotlib.pyplot as plt
+import pickle
 
 # Static Variables
 nodes = sys.argv[1].split()
+log_dir = sys.argv[2]
 nodes = list(map(int, nodes))
 nodes.sort()
 PORT = 5000
 MY_IP = '10.42.0.1'
-MSG_LEN = 100
+MSG_LEN = 512
 NUM_TESTS = 10
 
 print("Starting experiment with nodes: ", nodes)
@@ -41,6 +43,7 @@ broadcaster = udp.UdpBroadcaster(MY_IP)
 msgs = messages.gen_messages(len(nodes), MSG_LEN)
 
 # set up stats
+tests = []
 rounds = []
 round_time = []
 lost_msgs = []
@@ -63,18 +66,29 @@ for test in range(NUM_TESTS):
 
     while (len(toSend) > 0):
         rnd += 1
+
+        #if (rnd > 20):
+        #    print("spiking with nodes", messages.get_nodes(toSend[0]))
+
         for message in toSend:
             broadcaster.send(message, PORT)
             sent += 1
 
         sleep(0.025)
+        for i in range(len(nodes)):
+            if (acks.acks[i][i] == 1):
+                lost_by_owner += 1
+
         toSend = algorithms.reduceMessages(msgs, acks.acks, "rr")
 
     round_stop = time()
 
+    print("Finished round", test, "in", (round_stop - round_start), "seconds")
+
     sleep(1)
     acks.reset()
 
+    tests.append(test)
     rounds.append(rnd)
     round_time.append((round_stop - round_start) * 1000)
     lost_msgs.append(lost)
@@ -86,20 +100,27 @@ acks.stop()
 
 print("Rounds", rounds)
 print("Round Times", round_time)
+print("Msgs Sent", msgs_sent)
 print("Msgs Lost", lost_msgs)
 print("Msgs Lost by Owner", lost_by_owner_msgs)
+
+pickleInfo = {"Rounds":rounds, "Round Times":round_time, "Msgs Sent":msgs_sent, "Msgs Lost":lost_msgs,"Msgs Lost by Owner":lost_by_owner_msgs}
+pickle.dump(pickleInfo, open("{}/results.pkl".format(log_dir),"wb"))
+
 
 plt.plot(round_time)
 plt.title("Test Time")
 plt.xlabel("Test number")
 plt.ylabel("Time (ms)")
-plt.savefig('test_time.png')
+plt.savefig('{}/test_time.png'.format(log_dir))
+plt.close()
 
-plt.plot(msgs_sent)
-plt.title("Messages Sent")
+plt.plot(tests, msgs_sent, 'b', tests, lost_by_owner_msgs, 'r', tests, lost_msgs, 'g')
+plt.title("Number of Messages")
 plt.xlabel("Test number")
 plt.ylabel("Number of messages")
-plt.savefig('messages_sent.png')
+plt.savefig('{}/messages.png'.format(log_dir))
+plt.close()
 
 print("Testing complete.")
 
