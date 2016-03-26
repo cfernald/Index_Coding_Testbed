7cbe0ef62db07cec0a034d07ee18086ecf2f73d9
Chris Fernald <chris.fernald@utexas.edu>
2016-03-25 20:43:28 -0500

    Starting refactoring to use new decoded object and added decode_manager.py

---
+++ python/decode_manager.py
@@ -0,0 +1,70 @@
+import decoding, encoding, messages
+
+class DecodeManager:
+    'Handles the state info for decoding and runs decoding'
+
+    def __init__(self, numNodes): 
+        self.numNodes = numNodes
+        self.reset()
+
+    def reset(self):
+        self.coeffs = []
+        self.decoding_steps = []
+        seld.side_info = {}
+        self.encoded = []
+
+    def addMessage(self, raw_msg):
+        nodes = messages.get_nodes(raw_msg)
+        data = messages.get_data(raw_msg)
+
+        num_coeffs = 0
+        for node in nodes:
+            if node != 0:
+                num_coeffs += 1
+           
+        # update th active steps
+        for i in range(len(self.decoding_steps)):
+            self.decoding_steps[i].append(0)
+
+        # create our steps list
+        steps = ([0] * len(self.encoded)) + [1]
+
+        if num_coeffs > 1: #TODO do quick decoding for side_info stuff
+            #decode what we can first
+            for msgId in range(len(nodes)):
+                if (nodes[msgId] != 0):
+                    if msgId in self.side_info:
+                        
+                        # We already have this msgId decoded, lets extract it
+                        num_msgId = nodes[msgId]
+                        mult = self.sife_info[msgId][1]
+                        steps_msgId = self.side_info[msgId][0]
+                        
+                        # update the orig_msg matrix
+                        for i in range(len(nodes)):
+                            nodes[i] *= mult
+                        
+                        # update the steps list
+                        for i in range(len()):
+
+                        
+                            
+                        #TODO subtract out the steps * the coeffsicint of msgId
+                           
+                    
+            
+            # Add new coeffs row
+            self.coeffs.append(nodes)
+
+            # add the new decing steps and raw_msg
+            self.decoding_steps.append(steps) 
+        
+        else:
+            # This is a single message encoding
+            for i in range(len(nodes)):
+                if (nodes[i] != 0):
+                    # add the elimination steps array as well as the coefficient of our single message data to be divided by at decode time
+                    self.side_info[i] = tuple([], nodes[i])
+                        
+      
+        self.encoded.append(raw_msg)
--- python/experiment_Node.py
+++ python/experiment_Node.py
@@ -3,23 +3,29 @@ import socket, sys, messages
 from udp import *
 from ack_handler import *
 
-if (len(sys.argv) < 2):
-    print("Usage: node.py {id}")
+if (len(sys.argv) < 3):
+    print("Usage: node.py {id} {num nodes}")
     exit(1)
 
 me = int(sys.argv[1])
+num_nodes = int(sys.argv[2])
 
 ack_sender = AckSender("10.42.0.1")
 rec = UdpReceiver(5000)
+decoder = DecodeManager(num_nodes)
+last_rid = -1
 
-sideInfo = dict()
 while True:
+    # Get a message
     msg = bytearray(rec.rec(1024))
-    data = messages.get_data(msg)
-    nodes = messages.get_nodes(msg)
+    rid = messages.get_round(msg)
 
-    if len(nodes) == 1:
-        ack_sender.ack(me, nodes[0])
-        sideInfo[nodes[0]] = data
-    else:
-        print("we haven't implemented decoding yet")
+    if (rid != last_rid):
+        decoder.reset()
+        last_rid = rid
+
+    new_decoded = decoder.addMessage(msg)
+
+    for decoded in new_decoded:
+        ack_sender.ack(me, decoded)
+
